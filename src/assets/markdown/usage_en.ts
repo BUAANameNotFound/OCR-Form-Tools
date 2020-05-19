84e000fe13dc4e1b059de4e6a94028634d5d3143
lzhmarkjen <774028218@qq.com>
2020-05-19 22:32:17 +0800

    fea:初步完成introduction comment #47

---
+++ src/assets/markdown/usage_en.ts
@@ -0,0 +1,57 @@
+export const usageEn = "Hello, this blog will show you how to use the tool we build. [项目链接](<https://github.com/BUAANameNotFound/OCR-Form-Tools>)\n" +
+    "\n" +
+    "### 1. Establish new project\n" +
+    "\n" +
+    "First, you should establish a new project. Note the project name is very important, for you have to use it to build your work folder where you can store all of your data! We have simplified the procedure to help people who don't have an Azure account to use our tools.\n" +
+    "\n" +
+    "![](https://img2020.cnblogs.com/blog/1975516/202004/1975516-20200429190027445-182657514.png)" +
+    "\n" +
+    "\n" +
+    "\n" +
+    "\n" +
+    "\n" +
+    "### 2. Upload template file (pdf)\n" +
+    "\n" +
+    "Second, you can upload your template pdf file in this page!\n" +
+    "\n" +
+    "![](https://img2020.cnblogs.com/blog/1975516/202004/1975516-20200429001701320-494215715.png)" +
+    "\n" +
+    "\n" +
+    "\n" +
+    "\n" +
+    "\n" +
+    "\n" +
+    "### 3. Marking label\n" +
+    "\n" +
+    "The marking page is the same as the original one!\n" +
+    "To mark a label, you can just press the \"Backspace\" on your keyboard. \n" +
+    "Double clicks represent the diagonal and a box will appear, then you can attach the box to a tag.\n" +
+    "\n" +
+    "Then you should click a tag in the tag bar. After that, you should choose \"generate\" and an attribute in the toolbox. Now, the attribute we have choosen can be seen through the drop-down menu, such as name, date, phone number, etc. Later we will expand more attributes.\n" +
+    "\n" +
+    "When finishing these steps, you can use the 'cloud' (at the upper right corner) button to upload the marked JSON file. Then you can go to next step.\n" +
+    "\n" +
+    "![](https://img2020.cnblogs.com/blog/1975516/202004/1975516-20200429001716761-905760401.png)" +
+    "\n" +
+    "\n" +
+    "### 4. Generate new pdf\n" +
+    "\n" +
+    "In the data page, you should input the number of pdf you want to generate and click \"generate\" button. After the data is generated, the content of each generated pdf will be displayed on this page.In order to make the generated data look more real, we randomly disturb the position of data when generating each field.You can download the generated pdf and corresponding JSON data by clicking the download button.\n" +
+    "![](https://img2020.cnblogs.com/blog/1975516/202004/1975516-20200429001733621-185229044.png)" +
+    "\n" +
+    "Above is all the process.\n" +
+    "thanks!\n" +
+    "*****\n" +
+    "### Generation comparison\n" +
+    "**This is a comparison of the two generated files.**\n" +
+    "![](https://img2020.cnblogs.com/blog/1975516/202004/1975516-20200429082452127-2024541038.jpg)" +
+    "\n" +
+    "\n" +
+    "\n" +
+    "**This picture shows \"address\" 、 \"name\" 、 \"phone\".**\n" +
+    "![](https://img2020.cnblogs.com/blog/1975516/202005/1975516-20200505193224576-53230330.png)" +
+    "\n" +
+    "\n" +
+    "### Presentation video\n" +
+    "[video](https://www.bilibili.com/video/bv1Lz411z7D6)\n" +
+    "*****";
--- src/react/components/shell/helpMenu.tsx
+++ src/react/components/shell/helpMenu.tsx
@@ -2,28 +2,67 @@
 // Licensed under the MIT license.
 
 import React from "react";
-import { FontIcon } from "office-ui-fabric-react";
-import { strings } from "../../../common/strings";
+import {FontIcon} from "office-ui-fabric-react";
+import {Modal, ModalBody, ModalHeader} from "reactstrap";
+import {strings} from "../../../common/strings";
 import "./helpMenu.scss";
+import {markdown} from "markdown";
+import {usageEn} from "../../../assets/markdown/usage_en";
 
-export interface IHelpMenuProps {}
-export interface IHelpMenuState {}
+export interface IHelpMenuProps {
+}
+
+export interface IHelpMenuState {
+    helpModalOpen: boolean;
+}
 
 export class HelpMenu extends React.Component<IHelpMenuProps, IHelpMenuState> {
+    public state: IHelpMenuState = {
+        helpModalOpen: false,
+    };
 
     private akaMsLink = "https://aka.ms/form-recognizer/docs/label";
+    private markdownContent = markdown.toHTML(usageEn);
 
     public render() {
         return (
-            <a
-                className={"help-menu-button"}
-                title={strings.titleBar.help}
-                href={this.akaMsLink}
-                role="button"
-                target="_blank"
-                rel="noopener noreferrer">
-                <FontIcon iconName="Help" />
-            </a>
+            <div>
+                <a
+                    className={"help-menu-button"}
+                    title={strings.titleBar.help}
+                    onClick={this.handleClickHelpButton}
+                    role="button"
+                    target="_blank"
+                    rel="noopener noreferrer">
+                    <FontIcon iconName="Help"/>
+                </a>
+                <Modal isOpen={this.state.helpModalOpen}
+                       onOpened={this.renderMarkdownContent}
+                       className="help-menu-modal">
+                    <ModalHeader
+                        toggle={this.handleCloseModal}
+                        close={
+                            <button className="close" onClick={this.handleCloseModal}>&times;</button>
+                        }>
+                        Page Help
+                    </ModalHeader>
+                    <ModalBody>
+                        <div id={"markdown-content"}/>
+                    </ModalBody>
+                </Modal>
+            </div>
         );
+    }
+
+    private handleClickHelpButton = () => {
+        this.setState({helpModalOpen: true});
+    }
+
+    private handleCloseModal = () => {
+        this.setState({helpModalOpen: false});
+    }
+
+    private renderMarkdownContent = () => {
+        document.getElementById("markdown-content").innerHTML = this.markdownContent;
     }
 }
